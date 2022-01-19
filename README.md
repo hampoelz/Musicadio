b8e69f009563376623762df2f2e777e479ec6351
Rene Hampölz <rene@hampoelz.net>
2022-01-19 18:05:09 +0100

    build: 👷 Setup auto-build structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# 🎶 Musicadio Builds
+### [🚧 Nightly](https://hampoelz.github.io/Musicadio/nightly/)
+### [🏗️ Beta](https://hampoelz.github.io/Musicadio/beta/)
+### [🏠 Stable](https://hampoelz.github.io/Musicadio/stable/)
---
+++ beta/workbox.js
@@ -0,0 +1,10 @@
+//TODO: Upgrade Workbox
+self.importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
+
+workbox.loadModule("workbox-strategies");
+workbox.loadModule("workbox-routing");
+
+workbox.routing.registerRoute(
+    new RegExp("/.*"),
+    new workbox.strategies.NetworkFirst()
+);
