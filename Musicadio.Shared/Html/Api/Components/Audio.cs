dff23ccd15663c710e85f555568e60a0fb9844cd
Rene Hampölz <rene@hampoelz.net>
2022-01-19 18:24:57 +0100

    chore: :tada: Migrate project to new repo

---
+++ Musicadio.Shared/Html/Api/Components/Audio.cs
@@ -0,0 +1,14 @@
+using Uno.UI.Runtime.WebAssembly;
+
+namespace Musicadio.Shared.Html.Api.Components
+{
+    /// <summary>
+    ///     The <see cref="Audio" /> element is used to embed sound content in documents. It can also
+    ///     be the destination for streamed media.
+    /// </summary>
+    [HtmlElement("audio")]
+    public class Audio : MediaElement
+    {
+        // TODO: Create Audio-Element
+    }
+}
---
+++ Musicadio.Shared/Html/Api/Components/IFilePicker.cs
@@ -0,0 +1,10 @@
+namespace Musicadio.Shared.Html.Api.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IFilePicker
+    {
+        // TODO: Create FilePicker Interface
+    }
+}
